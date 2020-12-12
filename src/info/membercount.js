3d369642fadf8940b3163d74bb72f60a8ecdc486
Lazyy233 <xpierce@hotmail.com>
2020-12-12 01:03:36 +0000

    Initial commit

---
+++ src/info/membercount.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "membercount",
+    category: "info",
+    description: "shows server information",
+    run: (client, message, args) => {
+
+      message.delete()
+
+        let Embed = new Discord.MessageEmbed()
+            .setColor(0xdfa914)
+            .setTitle("Member Count:")
+            .setColor(0x5126c7)
+            .setDescription(`${message.guild.memberCount}`)
+        message.channel.send(Embed);
+    }
+}
---
+++ src/info/serverinfo.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js")
+
+module.exports = {
+    name: "serverinfo",
+    category: "info",
+    description: "shows server information",
+    run: (client, message, args) => {
+
+      message.delete()
+
+        let Embed = new Discord.Message()
+            .setColor(0xdfa914)
+            .setTitle("Server Info")
+            .setThumbnail(message.guild.iconURL)
+            .setColor(0x5126c7)
+            .setAuthor(`${message.guild.name}`, message.guild.iconURL)
+            .addField("**Guild Name:**", `${message.guild.name}`)
+            .addField("**Guild Owner:**", `${message.guild.owner}`)
+            .addField("**Created On:**", `${message.guild.createdAt}`)
+            .addField("**Member Count:**", `${message.guild.memberCount}`, true)
+            .addField("**Role Count:**", `${message.guild.roles.size}`, true)
+            .addField("**Channel Count:**", `${message.guild.channels.size}`, true)
+        message.channel.send(Embed);
+    }
+}
